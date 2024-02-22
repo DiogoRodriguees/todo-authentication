ae960deb901e09dadbeabc17fcfb891b408a7b5b
diogorodriguees <diogorodrigueslife@gmail.com>
2024-02-22 20:00:05 -0300

    :sparkles: compare hashs

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,8 +1,8 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 import { UserCredentials } from 'src/classes/dtos/UserCredentialsDTO';
 import { UserService } from './UserService';
-
 @Injectable()
 export class AuthService {
   constructor(
@@ -12,11 +12,13 @@ export class AuthService {
 
   async signIn(credentials: UserCredentials): Promise<{ access_token: string }> {
     const user = await this.usersService.findOne(credentials.email);
-    if (user?.password !== credentials.password) {
-      throw new UnauthorizedException();
-    }
+    const saltOrRounds = 10;
+    const hash = await bcrypt.hash(credentials.password, saltOrRounds);
+    const isMatch = await bcrypt.compare(credentials.password, hash);
 
-    const payload = { sub: user.id, username: user.name };
+    if (!isMatch) throw new UnauthorizedException();
+
+    const payload = { id: user.id, username: user.name };
 
     return {
       access_token: await this.jwtService.signAsync(payload),
